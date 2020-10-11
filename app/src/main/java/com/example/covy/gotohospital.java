aceac21a510e27a1b574aff59b15b352df59ec36
rjlee03 <67404792+rjlee03@users.noreply.github.com>
2020-10-11 15:51:43 -0700

    Edited final project

--- app/src/main/java/com/example/covy/gotohospital.java
+++ app/src/main/java/com/example/covy/gotohospital.java
@@ -2,7 +2,10 @@ package com.example.covy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class gotohospital extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class gotohospital extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gotohospital);
+    }
+
+    public void search(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("http://www.vch.ca/covid-19/covid-19-testing"));
+        startActivity(intent);
     }
 }
